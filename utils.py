7bb1a5d4aac516b0077ec223e84278814b4fc08f
Metin Senturk <metinsenturk@me.com>
2019-12-10 00:55:42 -0500

    assignment 1 - ready

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from PIL import Image
 
@@ -30,7 +31,8 @@ def download_image_from_url(url, directory):
     """
     response = requests.get(url, stream=True)
     if response.status_code == 200:
-        filepath = os.path.join(directory, '{{filename}}')
+        filename = os.path.basename(url)
+        filepath = os.path.join(directory, f'{filename}')
         with open(filepath, 'wb') as f:
             f.write(response.content)
     
@@ -39,18 +41,33 @@ def build_link_list(client_id, num_of_images):
     """
     builds a list of image links.
     """
-    link_list = []
-    response = requests.get(
-        'https://api.imgur.com/3/gallery/random/random/', 
-        headers={'Authorization': f'Client-ID {client_id}'}
-    )
+    cnt = 0
+    url_list = []
 
-    if response.status_code == 200:
-        with open() as f:
-            link = f.read()
-            link_list.append(link)
+    try:
+        while(cnt < num_of_images):
+            i = 1
+            response = requests.get(
+                f'https://api.imgur.com/3/gallery/r/nature/{i}', 
+                headers={'Authorization': f'Client-ID {client_id}'},
+                stream=True
+            )
+
+            if response.status_code == 200:
+                data_list = json.loads(response.content)['data']
+                url_list.extend([
+                    i['link']
+                    for i in data_list 
+                    if 'type' in i and 
+                    i['type'] in ('image/png', 'image/jpeg')
+                ])
+                cnt += len(url_list)
+                i += 1
+    except:
+        print('api limit reached!')
+        
     
-    return link_list
+    return url_list
 
 
 def create_thumbnail(size, path):
@@ -58,14 +75,17 @@ def create_thumbnail(size, path):
     create resized version of the image path given, with the same name 
     extended with _thumbnail.
     """
-    # create thumbnail
-    image = Image.open(path)
-    image.thumbnail(size)
+    try:
+        # create thumbnail
+        image = Image.open(path)
+        image.thumbnail(size)
 
-    # create path for thumbnail
-    dir_images = os.path.join(path)
-    filename, extension = os.path.splitext(path)
-    new_filename = os.path.join('{}{}{}'.format(filename, '_thumbnail', extension))
+        # create path for thumbnail
+        dir_images = os.path.join(path)
+        filename, extension = os.path.splitext(path)
+        new_filename = os.path.join('{}{}{}'.format(filename, '_thumbnail', extension))
 
-    # save thumbnail
-    image.save(new_filename)
+        # save thumbnail
+        image.convert('RGB').save(new_filename)
+    except:
+        'image error'
